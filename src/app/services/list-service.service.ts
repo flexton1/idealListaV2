83e1268542b0c2cf186867352ad2935ccb026857
Harun J <harunjasarevic00@gmail.com>
2022-04-25 02:44:29 +0200

    yes

--- src/app/services/list-service.service.ts
+++ src/app/services/list-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Track } from 'ngx-audio-player/lib/model/track.model';
+import { Track } from '../components/audio-player/track';
 
 @Injectable({
   providedIn: 'root'
